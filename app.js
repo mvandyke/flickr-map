ac0713d084715668813d94288b9a00b9a4ec0430
joelongstreet <joelongstreet@gmail.com>
2013-12-17 16:08:36 -0600

    rename controllers to libs since they're not really controllers

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express');
 var http    = require('http');
 var path    = require('path');
 var app     = express();
-var flickr  = require('./controllers/flickr');
-var db      = require('./controllers/db');
-var socket  = require('./controllers/socket');
+var db      = require('./libs/db');
+var socket  = require('./libs/socket');
 
 process.env.PORT = process.env.PORT || 3000;
 app.set('port', process.env.PORT);
