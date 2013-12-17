6c3a5a3db071cab40ac5f23acf8dc9d2474d523d
joelongstreet <joelongstreet@gmail.com>
2013-12-16 19:26:31 -0600

    can now plot more than one date. code cleanup

--- public/javascripts/example.js
+++ public/javascripts/example.js
@@ -1,30 +1,38 @@
-var createMarker = function(data){
-  L.mapbox.markerLayer({
-    type : 'Feature',
-    geometry : {
+var createMarker = function(data, map){
+  var marker = L.mapbox.markerLayer({
+    type        : 'Feature',
+    geometry    : {
         type        : 'Point',
         coordinates : [data.longitude, data.latitude]
     },
-    properties : {
+    properties  : {
         title           : data.title,
         description     : '...',
         'marker-color'  : '#f0a'
     }
   }).addTo(map);
+
+  return marker;
 };
 
 
+var clearMarkers = function(markers){
+  markers.forEach(function(marker){
+    marker.clearLayers();
+  });
+};
+
 
 window.onload = function(){
+  var markers   = [];
+  var map       = L.mapbox.map('map', 'examples.map-20v6611k');
+  window.socket = io.connect('http://localhost');
 
-  var socket = io.connect('http://localhost');
   socket.on('photos', function(data){
     data.forEach(function(item){
-      createMarker(item);
-    })
+      markers.push(createMarker(item, map));
+    });
   });
 
   socket.emit('getPhotosByDate', '2013-08-06');
-
-  window.map = L.mapbox.map('map', 'examples.map-20v6611k');
 };
