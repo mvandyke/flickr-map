082e2874242e6b7b9cf1ff4d87a75f392f9e0161
joelongstreet <joelongstreet@gmail.com>
2013-12-17 13:50:11 -0600

    dump the dirty db and begin to use redis

--- controllers/db.js
+++ controllers/db.js
@@ -1,42 +1,21 @@
-var dirty   = require('dirty');
+var redis   = require('redis');
 var flickr  = require('./flickr');
-var photos  = dirty('photos.db');
-
-// Since we're using dirty db, it's imperative
-// that we keep our data lite and clean up our
-// photos before inserting into dirty
-var save = function(date, results){
-  var photoToSave = [];
-  results.forEach(function(photo){
-    photoToSave.push({
-      id        : photo.id,
-      owner     : photo.owner,
-      title     : photo.title,
-      latitude  : photo.latitude,
-      longitude : photo.longitude,
-      farm      : photo.farm,
-      server    : photo.server,
-      secret    : photo.secret
-    });
-  });
-
-  photos.set(date, photoToSave);
-  return photoToSave;
-};
+var client  = redis.createClient();
 
 
 exports.fetch = function(date, next){
-  var results = photos.get(date);
-
-  if(results){
-    next(results);
-  } else {
-    flickr.fetch(date, function(apiResults){
-      if(Object.prototype.toString.call(apiResults) === '[object Array]'){
-        next(save(date, apiResults));
-      } else{
-        next([]);
-      }
-    });
-  }
+  client.get(date, function(err, results){
+    if(results){
+      next(results);
+    } else {
+      flickr.fetch(date, function(apiResults){
+        if(Object.prototype.toString.call(apiResults) === '[object Array]'){
+          client.set(date, apiResults);
+          next(apiResults);
+        } else{
+          next([]);
+        }
+      });
+    }
+  });
 };
