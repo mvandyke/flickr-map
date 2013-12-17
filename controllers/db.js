6ba7ef6398dbcc324c55690334718279d7dbd530
joelongstreet <joelongstreet@gmail.com>
2013-12-17 15:54:32 -0600

    stringify and parse data before inserting into db

--- controllers/db.js
+++ controllers/db.js
@@ -6,11 +6,11 @@ var client  = redis.createClient();
 exports.fetchPhotosByDate = function(date, next){
   client.get(date, function(err, results){
     if(results){
-      next(results);
+      next(JSON.parse(results));
     } else {
       flickr.fetchPhotosByDate(date, function(apiResults){
         if(apiResults instanceof Array){
-          client.set(date, apiResults);
+          client.set(date, JSON.stringify(apiResults));
           next(apiResults);
         } else{
           next([]);
